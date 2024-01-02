299c407518cadee7ca77ef12ff5e33406ff4ab69
lizhihai <786710726@qq.com>
2024-01-02 09:32:20 +0800

    first commit

---
+++ src/main/java/com/source/read/ReadApplication.java
@@ -0,0 +1,13 @@
+package com.source.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadApplication.class, args);
+    }
+
+}
